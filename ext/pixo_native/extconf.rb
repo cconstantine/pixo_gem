0fa55657d2437607e6a8d2fa2d1e3184ad94148a
Chris Constantine <cconstan@gmail.com>
2017-09-03 22:13:01 -0700

    closer?

---
+++ ext/pixo_native/extconf.rb
@@ -0,0 +1,23 @@
+# require 'rbconfig'
+
+# ruby_executable = File.join(RbConfig::CONFIG['bindir'], RbConfig::CONFIG['RUBY_INSTALL_NAME'] + RbConfig::CONFIG['EXEEXT'])
+
+# generator = ''
+
+# if RbConfig::CONFIG['host_os'] =~ /mingw/
+#   generator = '-G "MSYS Makefiles"'
+# elsif RbConfig::CONFIG['host_os'] =~ /cygwin/
+#   generator = '-G "Unix Makefiles"'
+# elsif RbConfig::CONFIG['host_os'] =~ /mswin/
+#   generator = '-G "NMake Makefiles"'
+# end
+
+# exec format('cmake %s -DCMAKE_INSTALL_PREFIX=../../ -DRUBY_EXECUTABLE=%s %s ../../ext/ ', generator, ruby_executable, ARGV.join(' '))
+
+
+
+require 'mkmf'
+puts "mkmf sadfkj"
+extension_name = 'pixo_native'
+dir_config(extension_name)
+create_makefile(extension_name)
--- native/pixo/extconf.rb
+++
@@ -1,5 +0,0 @@
-require 'mkmf'
-puts "mkmf sadfkj"
-extension_name = 'pixo_native'
-dir_config(extension_name)
-create_makefile(extension_name)
